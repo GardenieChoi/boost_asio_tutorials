b3f2fbd4bd0119e73bd9a9909764cc76cb052448
Gardenie Choi <gardenieChoi.developer@gmail.com>
2022-12-19 22:52:32 -0800

    Daytime.1

--- main.cpp
+++ main.cpp
@@ -1,71 +1,41 @@
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <boost/thread/thread.hpp>
-#include <boost/bind/bind.hpp>
 
-class printer
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
 {
-public:
-  printer(boost::asio::io_context& io)
-    : strand_(boost::asio::make_strand(io)),
-      timer1_(io, boost::asio::chrono::seconds(1)),
-      timer2_(io, boost::asio::chrono::seconds(1)),
-      count_(0)
+  try
   {
-    timer1_.async_wait(boost::asio::bind_executor(strand_,
-          boost::bind(&printer::print1, this)));
+    boost::asio::io_context io_context;
 
-    timer2_.async_wait(boost::asio::bind_executor(strand_,
-          boost::bind(&printer::print2, this)));
-  }
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints =
+      resolver.resolve("localhost", "daytime");
 
-  ~printer()
-  {
-    std::cout << "Final count is " << count_ << std::endl;
-  }
+    tcp::socket socket(io_context);
+    boost::asio::connect(socket, endpoints);
 
-  void print1()
-  {
-    if (count_ < 10)
+    for (;;)
     {
-      std::cout << "Timer 1: " << count_ << std::endl;
-      ++count_;
+      boost::array<char, 128> buf;
+      boost::system::error_code error;
 
-      timer1_.expires_at(timer1_.expiry() + boost::asio::chrono::seconds(1));
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-      timer1_.async_wait(boost::asio::bind_executor(strand_,
-            boost::bind(&printer::print1, this)));
+      if (error == boost::asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
     }
   }
-
-  void print2()
+  catch (std::exception& e)
   {
-    if (count_ < 10)
-    {
-      std::cout << "Timer 2: " << count_ << std::endl;
-      ++count_;
-
-      timer2_.expires_at(timer2_.expiry() + boost::asio::chrono::seconds(1));
-
-      timer2_.async_wait(boost::asio::bind_executor(strand_,
-            boost::bind(&printer::print2, this)));
-    }
+    std::cerr << e.what() << std::endl;
   }
-
-private:
-  boost::asio::strand<boost::asio::io_context::executor_type> strand_;
-  boost::asio::steady_timer timer1_;
-  boost::asio::steady_timer timer2_;
-  int count_;
-};
-
-int main()
-{
-  boost::asio::io_context io;
-  printer p(io);
-  boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
-  io.run();
-  t.join();
 
   return 0;
 }
