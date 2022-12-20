a058339f27127b81a48bf3d6763217185de2bd98
Gardenie Choi <gardenieChoi.developer@gmail.com>
2022-12-19 22:51:21 -0800

    Timer.5

--- main.cpp
+++ main.cpp
@@ -1,31 +1,71 @@
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind/bind.hpp>
 
-using namespace std;
-
-void print(const boost::system::error_code& , boost::asio::steady_timer* t, int* count)
+class printer
 {
-    if(*count < 5)
-    {
-        cout << *count << endl;
-        ++(*count);
+public:
+  printer(boost::asio::io_context& io)
+    : strand_(boost::asio::make_strand(io)),
+      timer1_(io, boost::asio::chrono::seconds(1)),
+      timer2_(io, boost::asio::chrono::seconds(1)),
+      count_(0)
+  {
+    timer1_.async_wait(boost::asio::bind_executor(strand_,
+          boost::bind(&printer::print1, this)));
 
-        t->expires_at(t->expiry() + boost::asio::chrono::seconds(5));
-        t->async_wait(boost::bind(print, boost::asio::placeholders::error, t, count));
+    timer2_.async_wait(boost::asio::bind_executor(strand_,
+          boost::bind(&printer::print2, this)));
+  }
+
+  ~printer()
+  {
+    std::cout << "Final count is " << count_ << std::endl;
+  }
+
+  void print1()
+  {
+    if (count_ < 10)
+    {
+      std::cout << "Timer 1: " << count_ << std::endl;
+      ++count_;
+
+      timer1_.expires_at(timer1_.expiry() + boost::asio::chrono::seconds(1));
+
+      timer1_.async_wait(boost::asio::bind_executor(strand_,
+            boost::bind(&printer::print1, this)));
     }
-}
+  }
+
+  void print2()
+  {
+    if (count_ < 10)
+    {
+      std::cout << "Timer 2: " << count_ << std::endl;
+      ++count_;
+
+      timer2_.expires_at(timer2_.expiry() + boost::asio::chrono::seconds(1));
+
+      timer2_.async_wait(boost::asio::bind_executor(strand_,
+            boost::bind(&printer::print2, this)));
+    }
+  }
+
+private:
+  boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+  boost::asio::steady_timer timer1_;
+  boost::asio::steady_timer timer2_;
+  int count_;
+};
 
 int main()
 {
-    boost::asio::io_context io;
-    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+  boost::asio::io_context io;
+  printer p(io);
+  boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
+  io.run();
+  t.join();
 
-    int count = 0;
-    t.async_wait(boost::bind(print, boost::asio::placeholders::error, &t, &count));
-    io.run();
-
-    cout << "Final Count is: " << count << endl;
-
-    return 0;
+  return 0;
 }
